ba1f035c177bb639bb90105c2d8afa6a1c3cb903
mdisena <mdisena@gmail.com>
2023-06-16 11:21:20 -0300

    pre entrega final a modificar

--- main.js
+++ main.js
@@ -1,63 +1,161 @@
 const productos = [
     {
-        id: "producto0001",
+        id: "001",
         nombre: "Pantalon Jean",
+        precio: 9500,
         descripcion: "Pantalon Maxi Jean claro",
         stock: 10,
+        urlImg: "https://i.ibb.co/c2ZZV1x/foto-1.jpg"
     },
     {
-        id: "producto0002",
+        id: "002",
         nombre: "Campera de Jean",
+        precio: 9500,
         descripcion: "Campera de Jean",
         stock: 10,
+        urlImg: "https://i.ibb.co/dM3sfW9/campera-jean.jpg"
     },
     {
-        id: "producto0003",
+        id: "003",
         nombre: "Remera Calaveras",
+        precio: 9500,
         descripcion: "Remera Calaveras negra",
         stock: 10,
+        urlImg: "https://i.ibb.co/K78h5hN/remera-gotica.jpg"
     },
     {
-        id: "producto0004",
+        id: "004",
         nombre: "Remera Ramones",
+        precio: 9500,
         descripcion: "Remera Ramones negra",
         stock: 10,
+        urlImg: "https://i.ibb.co/HVdysFW/remera-ramones.jpg"
     },
     {
-        id: "producto0005",
+        id: "005",
         nombre: "Campera de Cuero",
+        precio: 9500,
         descripcion: "Campera de cuero entallada roja",
         stock: 10,
+        urlImg: "https://i.ibb.co/jZWfbcB/campera-roja.jpg"
     },
     {
-        id: "producto0006",
+        id: "006",
         nombre: "Campera escosesa",
+        precio: 9500,
         descripcion: "Campera escosesa roja y negra con puños",
         stock: 10,
+        urlImg: "https://i.ibb.co/1vBYDQL/campera-escosesa.jpg"
     },
+    {
+        id: "007",
+        nombre: "Campera Jean con Parches",
+        precio: 9500,
+        descripcion: "Campera Jean negra con Parches",
+        stock: 10,
+        urlImg: "https://i.ibb.co/LhmcVcz/Campera-con-parches.jpg"
+    },
+    {
+        id: "008",
+        nombre: "Pantalón Chupin ",
+        precio: 9500,
+        descripcion: "Pantalón chupin negro",
+        stock: 10,
+        urlImg: "https://i.ibb.co/yR1g0w0/pantalon-chupin.jpg"
+    },
+    {
+        id: "009",
+        nombre: "Remera Boca ",
+        precio: 9500,
+        descripcion: "Remera Boca Blanca",
+        stock: 10,
+        urlImg: "https://i.ibb.co/HVDZX5N/remera-labios.jpg"
+    }
 ]
 
-const carrito = []
-let cantidadTotal = 0
 
+
+//Muestro Tarjetas
+const verProducto = ({ id, nombre, precio, descripcion, stock, urlImg }) => {
+    const contenedorTarjetas = document.querySelector("#contenedorTarjetas")
+    const tarjeta = document.createElement("div")
+    tarjeta.className = "tarjeta"
+    tarjeta.innerHTML = `
+                         <img src="${urlImg}"alt="">
+                         <div class="contenido">
+                            <h3>${nombre}</h3>
+                            <p>${descripcion}</p>
+                            <span><b>Precio:</b>${precio}$</span>
+                            </div>
+                            <form id="formCarrito${id}">
+                            <input name="id" type="hidden" value:${id}>
+                            <input name="cantidad" type="number" value="1" min="1" max="${stock}">
+                            <button type="submit"> Agregar al carriito </button>
+                            </form>
+                         `
+    contenedorTarjetas.append(tarjeta)
+}
+
+const verProductos = () => {
+    const contenedorTarjetas = document.createElement("div")
+    contenedorTarjetas.className = "contenedorTarjetas"
+    contenedorTarjetas.id = "contenedotTarjetas"
+    const contenedor = document.querySelector("#contenedor")
+    contenedor.append(contenedorTarjetas)
+    productos.forEach(producto => {
+        if (producto.stock != 0) {
+            verProducto(producto)
+        }
+    })
+}
+
+verProductos()
+
+const carrito = JSON.parse(localStorage.getItem("carrito")) ?? []
+//Agrego al Carrito
+const agregarCarrito = (id) => {
+    const formCarrito = document.querySelector("#formCarrito" + id)
+    formCarrito.addEventListener("submit", (e) => {
+        e.preventDefault()
+        const cantidad = e.target.children["cantidad"].value
+        carrito.push((
+            id,
+            cantidad
+        ))
+        localStorage.setItem("carrito",JSON,stringify(carrito));
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Your work has been saved',
+            showConfirmButton: false,
+            timer: 1500
+          })
+    })
+}
+
+/*
+const cantidadTotal=0
 productos.forEach((producto, index) =>{
-    const agregarAlCarrito = document.querySelector(`#${producto.id}`)
-    agregarAlCarrito.addEventListener("submit", (e) => {
+    const agregarALCarrito = document.querySelector(`#${producto.id}`)
+    agregarAlCarrito.addEventListener("submit",(e) => {
         e.preventDefault()
         const cantidad = parseInt(e.target.children["cantidad"].value)
         carrito.push({
             id: productos[index].id,
             nombre: productos[index].nombre,
-            descripcion: productos[index].descripcion,
             cantidad:cantidad
         })
         cantidadTotal = cantidadTotal + cantidad
         const numeroCarrito = document.querySelector("#numeroCarrito")
-        console.log(numeroCarrito)
         numeroCarrito.innerText =cantidadTotal
-        console.log(carrito)
-        console.log(agregarAlCarrito.id)
-        console.log(cantidadTotal)
-
     })
+    localStorage.setItem("carrito",JSON,stringify(carrito));
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: 'Your work has been saved',
+            showConfirmButton: false,
+            timer: 1500
+          })
 })
+*/
